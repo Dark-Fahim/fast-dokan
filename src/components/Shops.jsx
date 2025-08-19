08e6c91af811b151678eca3605f9f013bf753de2
Dark-Fahim <itsmunna1066@gmail.com>
2025-08-19 19:46:47 +0600

    redirect path added

--- src/components/Shops.jsx
+++ src/components/Shops.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+
 
 const Shops = () => {
     return (
         <div className='mt-20'>
-            <h1 className='text-5xl text-center font-bold'>Shops</h1>
-            <p className='text-5xl text-center font-bold'>Coming Soon..!!!!</p>
+            <h1 className='text-5xl text-center mt-5 font-bold'>Shops</h1>
+            <p className='text-5xl text-center mt-5 font-bold'>Coming Soon..!!!!</p>
         </div>
     );
 };
